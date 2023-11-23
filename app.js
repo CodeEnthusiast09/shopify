3f42fc1a9ce62812a6e7a09697f29fd87b337bc2
O'Brien <obrienadedapo15@gmail.com>
2023-11-23 10:13:20 +0100

    third commit

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 let remove = document.getElementsByClassName("info");
 
 const removed = () => {
-  console.log("work");
+  document.getElementsByClassName("info").innerHTML = '';
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+  const arrowDownButton = document.getElementById("arrow-down");
+  const arrowUpButton = document.getElementById("arrow-up");
+  const optionsContainer = document.querySelector(".options");
+  const firstOptionsDescription = document.querySelector(".options .des");
 
+  arrowDownButton.addEventListener("click", function () {
+    optionsContainer.classList.toggle("show-options");
+    firstOptionsDescription.classList.toggle("show-description");
+    arrowDownButton.style.display = "none";
+    arrowUpButton.style.display = "block";
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-      const arrowDownButton = document.getElementById('arrow-down');
-      const arrowUpButton = document.getElementById('arrow-up');
-      const optionsContainer = document.querySelector('.options');
-      const firstOptionsDescription = document.querySelector('.options .des');
-      
-      arrowDownButton.addEventListener('click', function () {
-        optionsContainer.classList.toggle('show-options');
-        firstOptionsDescription.classList.toggle('show-description');
-        arrowDownButton.style.display = 'none';
-        arrowUpButton.style.display = 'block';
-      });
-
-      arrowUpButton.addEventListener('click', function () {
-        optionsContainer.classList.toggle('show-options');
-        firstOptionsDescription.classList.toggle('show-description');
-        arrowDownButton.style.display = 'block';
-        arrowUpButton.style.display = 'none';
-      });
-    });
+  arrowUpButton.addEventListener("click", function () {
+    optionsContainer.classList.toggle("show-options");
+    firstOptionsDescription.classList.toggle("show-description");
+    arrowDownButton.style.display = "block";
+    arrowUpButton.style.display = "none";
+  });
+});
